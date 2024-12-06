9b7bdbd68795c660e2eae06029ae061bb1730550
Rafanambinantsoa <akutagawakarim@gmail.com>
2024-12-06 15:51:25 +0300

    Historique transfert globale de ramirandanva done

--- app/Http/Controllers/TodosConroller.php
+++ app/Http/Controllers/TodosConroller.php
@@ -4,57 +4,101 @@ namespace App\Http\Controllers;
 
 use App\Models\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TodosConroller extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $todoAll = Todo::get();
+
+        if ($todoAll == null) {
+            return response()->json([
+                'message' => 'nope le izy',
+            ]);
+        }
 
         return response()->json($todoAll);
 
     }
 
-    public function single(Todo $todo){
-        return response()->json($todo);        
+    public function single($todo)
+    {
+        $todo = Todo::find($todo);
+
+        if ($todo == null) {
+            return response()->json([
+                'message' => 'nope le izy',
+            ]);
+        }
+        return response()->json($todo);
     }
 
-    public function fafaina(Todo $todo){
-        $todo->delete();
-        if(!$todo){
+    public function fafaina($todo)
+    {
+        $todo = Todo::find($todo);
+        if ($todo == null) {
             return response()->json([
-                'message' => 'nope le izy'
+                'message' => 'nope le izy',
+            ]);
+        }
+
+        $todo->delete();
+        if (!$todo) {
+            return response()->json([
+                'message' => 'nope le izy',
             ]);
         }
         return response()->json([
-            'message' => 'wep man izy'
+            'message' => 'wep man izy',
         ]);
     }
 
-    public function update(Todo $todo , Request $request){
+    public function update(Todo $todo, Request $request)
+    {
+        $validators = Validator::make($request->all(), [
+            'content' => 'required',
+        ]);
+
+        if ($validators->fails()) {
+            return response()->json([
+                'message' => $validators->errors(),
+            ]);
+        }
+
         $todo->content = $request->content;
         $todo->save();
         return response()->json([
-            'message' => 'nice shot mon pote'
+            'message' => 'nice shot mon pote',
         ]);
     }
 
-    public function store(Request $request){
-        $content = $request->content ; 
-        $isDone = false; 
+    public function store(Request $request)
+    {
 
-        
+        $validators = Validator::make($request->all(), [
+            'content' => 'required',
+        ]);
+
+        if ($validators->fails()) {
+            return response()->json([
+                'message' => $validators->errors(),
+            ]);
+        }
+
+        $content = $request->content;
+        $isDone = false;
 
         if ($kim = Todo::create([
-            "content" => $content , 
-            "isDone" => $isDone
+            "content" => $content,
+            "isDone" => $isDone,
         ])) {
             return response()->json([
-                'message' => 'cool mo pote'
-            ] , 200);
+                'message' => 'cool mo pote',
+            ], 200);
         }
         return response()->json([
-            'message' => 'nope'
-        ] ,500);
-
+            'message' => 'nope',
+        ], 500);
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,45 +6,60 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     public function login(Request $request)
     {
-        $incomingsFields = $request->validate([
-            'email' => 'required' ,
-            'password' => 'required'
+        $validators = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required',
         ]);
 
-        if(Auth::attempt($incomingsFields)){
-            $user = User::where('email' , $incomingsFields['email'])->first();
+        if ($validators->fails()) {
+            return response()->json([
+                'message' => $validators->errors(),
+            ]);
+        }
+        $incomingsFields = $request->only(['email', 'password']);
+
+        if (Auth::attempt($incomingsFields)) {
+            $user = User::where('email', $incomingsFields['email'])->first();
             $token = $user->createToken('OurAppToken')->plainTextToken;
             return response()->json([
-                'token' => $token , 
-                'message' => 'Cool tu gere'
-            ] , 200);
-        }else{
+                'token' => $token,
+                'message' => 'Cool tu gere',
+            ], 200);
+        } else {
             return response()->json([
-                'message' => 'Wrong credentials'
+                'message' => 'Wrong credentials',
             ]);
         }
     }
     public function registration(Request $request)
     {
-        $credentials = $request->validate([
+        $validators = Validator::make($request->all(), [
             'email' => ['required', 'email'],
             'password' => ['required'],
             'name' => ['required'],
         ]);
+
+        if ($validators->fails()) {
+            return response()->json([
+                'message' => $validators->errors(),
+            ]);
+        }
+
         $data = User::create([
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'name' => $request->name
+            'name' => $request->name,
         ]);
 
         if (!$data) {
             return response()->json([
-                'message' => 'verifier ton credentials connard'
+                'message' => 'verifier ton credentials connard',
             ]);
         }
         return response()->json([
